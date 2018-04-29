c07d9d13321d569cbf8afaa7c880fa930a505ac3
dhruvit <naikd2@apps.tcnj.edu>
2018-04-29 10:37:47 -0400

    added bootstrap to create users and programs

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,22 @@
-class BootStrap {
+import com.thinksoas.system.*
+import com.thinksoas.data.*
 
+class BootStrap {
     def init = { servletContext ->
-    }
-    def destroy = {
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+        def userRole = new Role('ROLE_USER').save()
+        def admin = new User('admin', 'password').save()
+        def user = new User('user', 'password').save()
+        UserRole.create admin, adminRole, true
+        UserRole.create user, userRole, true
+
+        if (Program.list().isEmpty()) {
+            def program = new Program()
+            program.performanceTarget = 0.0
+            program.assessmentPeriod = 0.0
+            program.performanceThreshold = 0.0
+            program.deltaValue = 0.0
+            program.save()
+        }
     }
 }
